ac53adf1ca43fbd1227029e8d9cd50c528df6a10
alirfanyasin <risayunita21@gmail.com>
2023-07-22 21:43:28 +0700

    fix: footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -275,42 +275,6 @@
 
 
 
-    <footer class="bg-white mt-5 py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="{{ asset('assets/img/logo-secondary.png') }}" alt="" class="mt-4"
-                        width="250px">
-                </div>
-                <div class="col-md-2">
-                    <h5 class="fw-semibold text-main">INFORMASI</h5>
-                    <ul>
-                        <li><a href="" class="text-decoration-none list-none">Beranda</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Tentang Kami</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Layanan</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Artikel</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Testimonial</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-2">
-                    <h5 class="fw-semibold text-main">SUPPORT</h5>
-                    <ul>
-                        <li><a href="" class="text-decoration-none list-none">FAQ</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-2">
-                    <h5 class="fw-semibold text-main">SOSIAL MEDIA</h5>
-                    <ul>
-                        <li><a href="" class="text-decoration-none list-none">Instagram</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Twitter</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Facebook</a></li>
-                        <li><a href="" class="text-decoration-none list-none">Youtube</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-
+    <x-footer.footer />
 
 @endsection

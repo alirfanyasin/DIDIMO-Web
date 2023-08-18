506dda0f094e57845df5ef1f023c321c798cb7f4
alirfanyasin <risayunita21@gmail.com>
2023-08-18 13:32:33 +0700

    feat: template

--- resources/views/admin/artikel.blade.php
+++ resources/views/admin/artikel.blade.php
@@ -2,5 +2,9 @@
 @section('title', 'Artikel')
 
 @section('content')
-
+    {{-- Write your code in here --}}
 @endsection
+
+@push('js-libraries')
+    {{-- If using library JS, Write your code in here --}}
+@endpush
